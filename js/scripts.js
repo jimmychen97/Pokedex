bd24f2f4ad9fb7a56b70e1e0501157e5f7280c36
Jimmy Chen <jimmychen9785@gmail.com>
2022-02-12 20:21:35 -0800

    added height to pokemonList objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonList = [
-  { name: "Bulbasaur", type: ["Grass", "Poison"] },
-  { name: "Ivysaur", type: ["Grass", "Poison"] },
-  { name: "Venusaur", type: ["Grass", "Poison"] }
+  { name: "Bulbasaur", height: '0.7m', type: ["Grass", "Poison"] },
+  { name: "Ivysaur", height: '1m', type: ["Grass", "Poison"] },
+  { name: "Venusaur", height: '2m', type: ["Grass", "Poison"] }
 ];
 
 document.write(pokemonList[1].type[1]);

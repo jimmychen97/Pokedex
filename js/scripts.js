4958f94a3f01164964317240c4c7c667b2c0577d
Jimmy Chen <jimmychen9785@gmail.com>
2022-02-12 20:14:36 -0800

    Added pokemonList object array including three entries

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello world');
-let favoriteFood = 'Ramen';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bulbasaur", type: ["Grass", "Poison"] },
+  { name: "Ivysaur", type: ["Grass", "Poison"] },
+  { name: "Venusaur", type: ["Grass", "Poison"] }
+];
+
+document.write(pokemonList[1].type[1]);

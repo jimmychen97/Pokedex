940ab4bc8843cafebe3b483927041a4463df72ea
Jimmy Chen <jimmychen9785@gmail.com>
2022-02-14 20:28:57 -0800

    added conditional statement to write each pokemon on HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+// an object of pokemons
 let pokemonList = [
-  { name: "Bulbasaur", height: '0.7m', type: ["Grass", "Poison"] },
-  { name: "Ivysaur", height: '1m', type: ["Grass", "Poison"] },
-  { name: "Venusaur", height: '2m', type: ["Grass", "Poison"] }
+  { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+  { name: "Ivysaur", height: 1, type: ["Grass", "Poison"] },
+  { name: "Venusaur", height: 2, type: ["Grass", "Poison"] }
 ];
 
-document.write(pokemonList[1].type[1]);
+// displaying names and heights of each pokemon in the pokemonList
+// conditional if-else to check if a pokemon's height is above 1.5
+for(let i = 0; i< pokemonList.length; i++){
+  
+  if(pokemonList[i].height >= 1.5) {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+  } else {
+    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+  }
+  document.write("<br>");
+}
+
+
+

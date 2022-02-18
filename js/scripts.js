d563e4d52c0dcaf12358ee5bd36a38bf3470d524
Jimmy Chen <jimmychen9785@gmail.com>
2022-02-17 21:34:59 -0800

    Added forEach loop to replace for loop

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,22 @@ let pokemonList = [
 
 // displaying names and heights of each pokemon in the pokemonList
 // conditional if-else to check if a pokemon's height is above 1.5
-for(let i = 0; i< pokemonList.length; i++){
+// for(let i = 0; i< pokemonList.length; i++){
   
-  if(pokemonList[i].height >= 1.5) {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+//   if(pokemonList[i].height >= 1.5) {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
+//   } else {
+//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+//   }
+//   document.write("<br>");
+// }
+
+// forEach loop instead of for loop
+// arrow function that takes in one parameter, item and prints out the height property of each item
+pokemonList.forEach(item => {
+  if(item.height >= 1.5) {
+    console.log(item.name + " (height: " + item.height + ") - Wow, that's big!");
   } else {
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    console.log(item.name + " (height: " + item.height + ")");
   }
-  document.write("<br>");
-}
-
-
-
+});

8a8166deca5e585a9251288fc1e68a045eb4e88a
Jimmy Chen <jimmychen9785@gmail.com>
2022-02-17 21:50:34 -0800

    updated pokemonList with IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
-// an object of pokemons
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
-  { name: "Ivysaur", height: 1, type: ["Grass", "Poison"] },
-  { name: "Venusaur", height: 2, type: ["Grass", "Poison"] }
-];
+// an object of pokemons wrapped in IIFE
+let pokemonRepository = (function (){
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["Grass", "Poison"] },
+    { name: "Ivysaur", height: 1, type: ["Grass", "Poison"] },
+    { name: "Venusaur", height: 2, type: ["Grass", "Poison"] }
+  ];
 
-// displaying names and heights of each pokemon in the pokemonList
-// conditional if-else to check if a pokemon's height is above 1.5
-// for(let i = 0; i< pokemonList.length; i++){
-  
-//   if(pokemonList[i].height >= 1.5) {
-//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") - Wow, that's big!");
-//   } else {
-//     document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
-//   }
-//   document.write("<br>");
-// }
+  // return all items
+  function getAll(){
+    return pokemonList;
+  }
+
+  // add a single item to pokemonList
+  function add(pokemon){
+    pokemonList.push(pokemon);
+  }
+
+  // object with key/value pair
+  return {
+    getAll: getAll,
+    add: add
+  }
+})();
 
 // forEach loop instead of for loop
 // arrow function that takes in one parameter, item and prints out the height property of each item
-pokemonList.forEach(item => {
+pokemonRepository.getAll().forEach(item => {
   if(item.height >= 1.5) {
     console.log(item.name + " (height: " + item.height + ") - Wow, that's big!");
   } else {
